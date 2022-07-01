b1af23c744d8444b34cf066cd5e13a09416c6349
mar-romero <65137011+mar-romero@users.noreply.github.com>
2022-07-01 18:53:36 -0300

    Update ResumePage.js

--- src/Pages/ResumePage.js
+++ src/Pages/ResumePage.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Skills from '../Components/Skills';
 import { MainLayout} from '../styles/Layouts';
 import Resume from '../Components/Resume';
-
+import Particle from '../Components/Particle';
 function ResumePage() {
     return (
         <MainLayout>
+            <div className="particle-con">
+                <Particle />
+            </div>
             <Skills />
             <Resume />
         </MainLayout>
